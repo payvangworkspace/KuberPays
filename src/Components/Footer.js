cdfedca5453ab491b167f7c73aa2838a247abf79
Nitesh <niteshsaini1296@gmail.com>
2025-07-14 11:53:06 +0530

    Footer Logo Sized Increased

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,12 +9,14 @@ const Footer = () => {
         <div className="footer-left">
           <img className="logo logo-footer" src={`${process.env.PUBLIC_URL}/logo2.png`} />
           {/* <p>Swift Code: <strong>KUBERPAYMENT</strong></p> */}
+        <div className="footer-para-left">
           <p>
             <FaMapMarkerAlt className="icon" /> Kuber Pays, 113 Lakeshore Rd, West, Suite-163,<br />
              Mississauga ON L5H 1E9  Bifro House, Canada
           </p>
           <p className='phone-contact' onClick={() => (window.location.href = "tel:+1-647-619-0777")}><FaPhoneAlt className="icon"  /> +1-647-619-0777</p>
           <p className='phone-contact' onClick={() => (window.location.href = "mailto:info@kuberpayment.com")}><FaEnvelope className="icon" /> info@kuberpayment.com</p>
+        </div>
         </div>
 
         <div className="footer-right">
